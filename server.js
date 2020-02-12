0ea255fab747351841c2f39345756df07081db90
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2020-02-12 15:01:41 +0000

    setup express and add bcrypt for encription

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req,res) => {
     res.render('index.ejs', { name: 'Bart'})
@@ -9,7 +13,9 @@ app.get('/', (req,res) => {
 
 app.get('/login', (req, res) => {
     res.render('login.ejs')
-
+})
+app.post('/login', (req,res) => {
+    
 })
 app.get('/register', (req, res) => {
     res.render('register.ejs')
@@ -17,7 +23,7 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req,res) => {
-    
+    req.body.email
 })
 
 app.listen(3000)
